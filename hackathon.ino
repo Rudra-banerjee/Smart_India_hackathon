5e50ddb15a5e4777be53d7bdb89a44b4b9e92e8c
Rudra-banerjee <Rudra.banerjee98@gmail.com>
2017-11-12 22:35:13 +0530

    Updated LCD section
    To print various messages as per sensor readings

--- hackathon.ino
+++ hackathon.ino
@@ -6,6 +6,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 void setup() {
   Serial.begin(9600);
   lcd.begin(16,2);
+  lcd.clear();
   lcd.print("WELCOME");
   pinMode(A0,INPUT); //Moisture sensor reading pin A0
   pinMode(1,OUTPUT); // Motor output
@@ -22,17 +23,24 @@ void loop()
   int wl=analogRead(A1);
   void water_level()// taking input from water level sensor and controlling the motor
   {
-    if(wl>=900 && wl<1024)
+    if(wl>=900 && wl<1024){
     digitalWrite(1,LOW);
+    lcd.setCursor(0,0); // Sets the cursor to col 0 and row 0
+    lcd.print("Water Level:LOW"); // Prints Sensor Val: to LCD
+    }
     else 
     {
         if(wl>=600 && wl<900)        
         { 
           moisture_control(0)//call the function for checking the moisture level;
+            lcd.setCursor(0,0); // Sets the cursor to col 0 and row 0
+            lcd.print("Water Level:MED"); // Prints Sensor Val: to LCD
         }
         else
         {
-          moisture_control(1)//call the funtion for checking moisture level; 
+          moisture_control(1)//call the funtion for checking moisture level;
+            lcd.setCursor(0,0); // Sets the cursor to col 0 and row 0
+            lcd.print("Water Level:HIGH"); // Prints Sensor Val: to LCD
         }
     }
   }
@@ -41,10 +49,14 @@ void moisture_control(int wll) {
   val= analogRead(A0);
   Serial.println(val);
   if(val>800&& val<1024){
+    lcd.setCursor(0,1); // Sets the cursor to col 1 and row 0
+    lcd.print("MOTOR ON"); // Prints Sensor Val: to LCD
   motorOn(wll);
   }
   else{
     digitalWrite(1,LOW);
+    lcd.setCursor(0,1); // Sets the cursor to col 1 and row 0
+    lcd.print("MOTOR OFF"); // Prints Sensor Val: to LCD
   }
 
 }
